bf1b7c9e8c3617ddfc1c1add54f52caa23ecbd52
Brook <71164623+mrbrooklyn@users.noreply.github.com>
2022-02-09 10:44:12 +0700

    Navigation-Base
    Navigation-Base

---
+++ 2-9-2022/screens/ProductScreen.js
@@ -0,0 +1,54 @@
+//HomeScreen.js
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+} from 'react-navigation-header-buttons';
+
+const IoniconsHeaderButton = props => (
+  <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
+
+const ProduceScreen = ({navigation}) => {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title="menu"
+            iconName="menu"
+            onPress={() => navigation.openDrawer()}
+          />
+        </HeaderButtons>
+      ),
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title="register"
+            iconName="person-add"
+            onPress={() => alert('ลงทะเบียน')}
+          />
+        </HeaderButtons>
+      ),  
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Text>สินค้า</Text>
+    </View>
+  );
+};
+
+export default ProduceScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
